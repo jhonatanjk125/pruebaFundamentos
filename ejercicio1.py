1671507c76959e6657429039578f749011e77c79
jhonatanjk125 <jhonatanjk125@gmail.com>
2024-01-24 11:54:34 -0500

    Ejercicio 1

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+sum=0
+n=5
+for i in range(n):
+    current_input = int(input(f'Ingrese voltaje {i+1}: '))
+    sum += current_input
+avg = sum/n
+if avg>220:
+    print(f'<ALTO VOLTAJE= {avg}')
+else:
+    print(f'<VOLTAJE CORRECTO = {avg}')
